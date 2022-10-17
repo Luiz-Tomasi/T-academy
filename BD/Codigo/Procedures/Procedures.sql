10d5aea43250f12c853a708e0548ce1d4d62d998
Luiz Cirilo Tomasi Neto <f0fp1409@t-systems.com.br>
2022-10-17 16:40:25 -0300

    Modelo Atualizado e procedure de criar Marca adicionada

--- BD/Codigo/Procedures/Procedures.sql
+++ BD/Codigo/Procedures/Procedures.sql
@@ -1,3 +1,4 @@
+SET FOREIGN_KEY_CHECKS=0; #Permitir manipulação de foreign key
 #1: Pelo menos três para realizar cadastros
 DELIMITER $$
 create procedure cadastrarLocacaoLoja(IN endereco varchar(45), IN idLoja int, OUT msg varchar(45))
@@ -71,4 +72,107 @@ DELIMITER ;
 drop procedure cadastrarCarrinho;
 
 call cadastrarCarrinho("6", "1", "1", "1", @saida);
+
+
+#1: Pelo menos três para alterar
+DELIMITER $$
+create procedure alteraCartao(In idCartao int, IN saldo double, OUT msg varchar(45))
+BEGIN
+	if saldo < 0 then
+		set msg = "A quantia não pode ser negativa";
+	else
+		set msg = "Ok";
+	end if;
+    
+    if msg = "Ok" then 
+		update cartoes_loja as cj set cj.dinheiro = saldo where cj.idCartao = idCartao;
+        set msg = "Saldo alterado com sucesso";
+	end if;
+    
+    select msg;
+END $$
+DELIMITER ;
+
+call alteraCartao("1", "70000", @saida);
+select * from cartoes_loja where dinheiro = "70000";
+
+#2: Pelo menos três para alterar # Prenche uma vaga vazia 
+DELIMITER $$ 
+create procedure alteraLocacao(In endereco varchar(45), IN idLoja int, OUT msg varchar(45))
+
+BEGIN
+	declare id int;
+	if (select count(l.nome) from locacoes as lo inner join lojas as l on (lo.idLoja = idLoja) where isnull(lo.idLoja)) > 0 then
+		set msg = "Loja já alocada";
+	elseif (select count(lo.endereco) from locacoes as lo where lo.endereco = endereco and disponivel = 0) > 0 then
+		set msg = "Endereço já esta em uso";
+	else
+		set msg = "Ok";
+	end if;
+    
+    if msg = "Ok" then
+		set id = (select lo.idLocacao from locacoes as lo where lo.endereco = endereco);
+		update locacoes as lo set lo.idLoja = "33" and disponivel = 0 where lo.idLocacao = id;
+		set msg = "Alterado com sucesso";
+	end if;
+    
+    select msg;
+END $$
+DELIMITER ;
+
+insert into lojas values ("53", "NovaLoja");
+call alteraLocacao("B3", "53", @saida);
+select * from locacoes;
+
+#3: Pelo menos três para alterar
+DELIMITER $$ 
+create procedure alteraCategoria(In idCategoria int, IN nomeCategoria varchar(45), OUT msg varchar(45))
+BEGIN
+	if length(nomeCategoria) > 5 then
+		update categorias as ca set ca.nome = nomeCategoria where ca.idCategoria = idCategoria;
+        set msg = "Categoria Alterada com sucesso";
+	else
+		set msg = "Nome muito pequeno";
+	end if;
+    
+    select msg;
+END $$
+DELIMITER ;
+
+select * from categorias;
+call alteraCategoria("5", "Social", @saida);
+
+#1: Pelo menos três para deletar
+DELIMITER $$ 
+create procedure deletaCategoria(In idCategoria int, OUT msg varchar(45))
+BEGIN
+		delete from categorias where idCategoria = idCategoria;
+        set msg = "Categoria deletada com sucesso";
+    
+    select msg;
+END $$
+DELIMITER ;
+
+call deletaCategoria("5", @saida);
+select * from categorias;
+
+
+#1: Pelo menos três para deletar
+DELIMITER $$ 
+create procedure createMarca(In nomeMarca varchar(45), OUT msg varchar(45))
+BEGIN
+		if (select count(m.nome) from marcas as m where m.nome = nomeMarca) > 1 then
+			set msg = "Marca já cadastrada";
+		else
+			insert into marcas(nome) values (nomeMarca);
+            set msg = "Marca cadastrada com sucesso";
+		end if;
+        
+    select msg;
+END $$
+DELIMITER ;
+
+
+call createMarca("MarcaNova", @saida);
+select * from marcas where nome = "MarcaNova";
 
